e7658be074de7ef6dbe1703ba5ffc15cd89923a7
bestshield <33286246+bestshield@users.noreply.github.com>
2020-04-17 17:50:51 +0200

    Update testetstt.c

--- testetstt.c
+++ testetstt.c
@@ -6,14 +6,16 @@ int main(void)
 { 
 	int ERRORCODE; 
 	int waarde = 1 + 0;
-	printf("waarde = %d", waarde);
+	
 	if (waarde = 2)
 	{
 		ERRORCODE = 0;
 	}
 	if (waarde != 2)
 	{
-		ERRORCODE <= 1;
+		ERRORCODE = 1;
 	}
+	printf("waarde = %d", waarde);
+	printf("ERRORCODE = %d", ERRORCODE);
 	return ERRORCODE;   
 }
